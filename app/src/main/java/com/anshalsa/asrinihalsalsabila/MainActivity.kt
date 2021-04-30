71b2c9da814208a10836917ad00d46ee3d1d3440
Ziifauzii11 <57211455+Anshalsa@users.noreply.github.com>
2021-04-30 14:56:11 +0700

    18090087

---
+++ app/src/main/java/com/anshalsa/asrinihalsalsabila/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.anshalsa.asrinihalsalsabila
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anshalsa.asrinihalsalsabila.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
